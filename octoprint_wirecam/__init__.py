f5def5c98c29d3a8b34ac3fa8033152b5c5adc0c
Rob Willison <Rob@willison.co.uk>
2021-08-13 15:58:24 +0100

    first commit

---
+++ octoprint_wirecam/__init__.py
@@ -0,0 +1,69 @@
+# coding=utf-8
+from __future__ import absolute_import
+import flask
+import octoprint.plugin
+import serial
+
+### (Don't forget to remove me)
+# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
+# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
+# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
+# as necessary.
+#
+# Take a look at the documentation on what other plugin mixins are available.
+
+import octoprint.plugin
+
+class WirecamPlugin(octoprint.plugin.StartupPlugin,
+    octoprint.plugin.TemplatePlugin,
+    octoprint.plugin.BlueprintPlugin,
+    octoprint.plugin.AssetPlugin):
+
+    def on_after_startup(self):
+        self._logger.info("Hello World!")
+        self.serial = serial.Serial ("/dev/ttyS0", 9600)
+
+    def get_update_information(self):
+        return {}
+
+    def get_assets(self):
+     return dict(
+         js=["js/wirecam.js"]
+     )
+
+    @octoprint.plugin.BlueprintPlugin.route("/home", methods=["GET"])
+    def home(self):
+        self.serial.write(b'HOME')
+        return 'Homed'
+
+    @octoprint.plugin.BlueprintPlugin.route("/move", methods=["GET"])
+    def move(self):
+        x = flask.request.values['x']
+        y = flask.request.values['y']
+        z = flask.request.values['z']
+        self.serial.write(str.encode('C' + x + ',' + y + ',' + z)
+        return 'Moved'
+
+
+
+
+# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
+# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
+# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
+__plugin_name__ = "Wirecam Plugin"
+
+# Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
+# Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
+# compatibility flags according to what Python versions your plugin supports!
+#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
+#__plugin_pythoncompat__ = ">=3,<4" # only python 3
+__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+
+def __plugin_load__():
+    global __plugin_implementation__
+    __plugin_implementation__ = WirecamPlugin()
+
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
---
+++ octoprint_wirecam/static/js/wirecam.js
@@ -0,0 +1,50 @@
+/*
+ * View model for OctoPrint-Wirecam
+ *
+ * Author: You
+ * License: AGPLv3
+ */
+$(function() {
+    function WirecamViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        self.move = function() {
+          $.ajax({
+                url: "./plugin/wirecam/move",
+                type: "GET",
+                data: {x: $('#tab_plugin_wirecam #x').val(), y: $('#tab_plugin_wirecam #y').val(), z: $('#tab_plugin_wirecam #z').val()},
+                success: function (results) {
+                  console.log(results)
+                }
+            });
+        }
+
+        self.home = function() {
+          $.ajax({
+                url: "./plugin/wirecam/home",
+                type: "GET",
+                success: function (results) {
+                  console.log(results)
+                }
+            });
+        }
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: WirecamViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: ["settingsViewModel"],
+        // Elements to bind to, e.g. #settings_plugin_wirecam, #tab_plugin_wirecam, ...
+        elements: [ '#tab_plugin_wirecam' ]
+    });
+});
